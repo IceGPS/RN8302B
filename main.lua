366490b764db46f6353ac23e24eb45c342d50ae4
Axe <alex@icegps.com>
2023-08-28 23:28:24 +0800

    设置并启用SPI接口，但是返回值是-1，错误原因未知

--- main.lua
+++ main.lua
@@ -1,36 +1,39 @@
-
--- LuaTools需要PROJECT和VERSION这两个信息
 PROJECT = "RN8302B"
 VERSION = "0.0.1"
--- OTA 必须有产品KEY
 PRODUCT_KEY = ""
 
--- 引入必要的库文件(lua编写), 内部库不需要require，但是sys必须require
 sys = require("sys")
 sysplus = require("sysplus")
 
-----------------------------------------
--- 报错信息自动上报到平台,默认是iot.openluat.com
--- 支持自定义, 详细配置请查阅API手册
--- 开启后会上报开机原因, 这需要消耗流量,请留意
-if errDump then
-    --errDump.config(true, 600)
+--spi编号和CS管脚，请按实际情况修改
+local spiId = 0
+local cs = 14
+local cspin = gpio.setup(cs, 1)
+
+--收发数据
+local function sendRecv(data,len)
+    local r = ""
+    cspin(0)
+    if data then spi.send(spiId,data) end
+    if len then r = spi.recv(spiId,len) end
+    cspin(1)
+    return r
 end
-----------------------------------------
 
-
-function read8302()
-
-
-end
-
-
-function Task()
+local function Task()
     log.info("Main Task", "RN8302B")
 
+    local result = spi.setup(spiId,cs,0,0,8,100000)
+    print("open SPI",result)
+    if result ~= 0 then--返回值为0，表示打开成功
+        print("spi open error",result)
+    else
+        spi.close(spiId)
+    end
     while true do
         sys.wait(10000)
     end
+
 end
 
 sys.taskInit(Task)
